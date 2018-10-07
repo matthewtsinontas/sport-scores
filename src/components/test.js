177e27177d4b5d805e9b1eba4b8cdb2f2bfbbd28
Matthew Tsinontas <matthewtsinontas@gmail.com>
2018-10-07 23:43:46 +0100

    Enzyme adapter setup

---
+++ src/components/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Main from '.';
+
+describe('Main App', () => {
+  it('renders the Main component', () => {
+    const element = shallow(<Main />);
+    expect(element.exists()).toBe(true);
+  });
+});
