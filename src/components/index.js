5c6074cde5a127edb594a1ba400fc8f17efb7a54
Matthew Tsinontas <matthewtsinontas@gmail.com>
2018-10-07 23:36:25 +0100

    Final config setup

---
+++ src/components/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <p>Hello, I'm the main app!</p>
+    )
+  }
+
+}
+
+export default Main;
